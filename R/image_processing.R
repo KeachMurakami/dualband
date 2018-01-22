8c31ae33a1f53235feb68bb61585ecd2765bab68
MurakamiKeach <m.keach.11@gmail.com>
2018-01-22 13:32:37 +0900

    minor change in trim and overdraw: outside of the subscripts are ignored (without Caution <- changed?).

--- R/image_processing.R
+++ R/image_processing.R
@@ -2,19 +2,38 @@ show <- pri::view
 
 # image processing
 
+grids <-
+  function(x_cent, y_cent, size){
+    x <- (x_cent-size):(x_cent+size)
+    y <- (y_cent-size):(y_cent+size)
+    return(list(x, y))
+  }
+
 trim <-
   function(img, x_cent, y_cent, size){
-    img[(x_cent-size):(x_cent+size), (y_cent-size):(y_cent+size), , drop = F]
+    dims <- dim(img)
+    grid <- grids(x_cent, y_cent, size)
+    xxx <- grid[[1]][between(grid[[1]], 1, dims[1])]
+    yyy <- grid[[2]][between(grid[[2]], 1, dims[2])]
+    img[xxx, yyy, , drop = F]
   }
 
 trim2d <-
   function(img_2d, x_cent, y_cent, size){
-    img_2d[(x_cent-size):(x_cent+size), (y_cent-size):(y_cent+size)]
+    dims <- dim(img_2d)
+    grid <- grids(x_cent, y_cent, size)
+    xxx <- grid[[1]][between(grid[[1]], 1, dims[1])]
+    yyy <- grid[[2]][between(grid[[2]], 1, dims[2])]
+    img_2d[xxx, yyy]
   }
 
 overdraw <-
   function(img, x_cent, y_cent, size, by = 0){
-    img[(x_cent-size):(x_cent+size), (y_cent-size):(y_cent+size), ] <- by
+    dims <- dim(img)
+    grid <- grids(x_cent, y_cent, size)
+    xxx <- grid[[1]][between(grid[[1]], 1, dims[1])]
+    yyy <- grid[[2]][between(grid[[2]], 1, dims[2])]
+    img[xxx, yyy, ] <- by
     return(img)
   }
 
