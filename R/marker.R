771bb10a59972c3ec244481cc01edb3147339f65
MurakamiKeach <m.keach.11@gmail.com>
2018-02-28 18:56:20 +0900

    feat: major change for marker-based analyses.

--- R/marker.R
+++ R/marker.R
@@ -1,249 +1,249 @@
-# detect_marker
-
-get_marker <-
-  function(img, x, y, size, outer_size, percent = "95%", occupancy = .1, show_mapping = F){
-
-    trimed_img <-
-      trim(img, x, y, size)
-
-    img_bin <-
-      trimed_img %>%
-        pri::add_dimension() %>%
-        imager::threshold(percent) %>%
-        # thresh(., w = size * window, h = size * window, offset) %>%
-        .[,,,1] %>%
-        EBImage::fillHull()
-
-    marker_center <-
-      img_bin %>%
-      EBImage::bwlabel() %>%
-      array_branch(margin = 3) %>%
-      map(function(img_split){
-          marker_detect <-
-            computeFeatures.shape(img_split) %>%
-            as_tibble %>%
-            mutate(row_index = seq_along(s.area), area = s.area,
-                   size_for_calc = round(s.radius.mean * .25)) %>%
-            arrange(desc(area)) %>%
-            filter(area > size^2 * occupancy^2)
-
-          if(nrow(marker_detect) != 1){
-            cat(paste0("markers may be splitted. detected ",
-                       nrow(marker_detect),
-                       " locations in binarized image.\n"))
-          }
-
-          computeFeatures.moment(img_split) %>%
-            as_tibble %>%
-            slice(marker_detect$row_index[1]) %>%
-            transmute(xx = round(m.cx), yy = round(m.cy), ss = marker_detect$size_for_calc[1])
-    })
-
-    marker_int <-
-      trimed_img %>%
-      array_branch(margin = 3) %>%
-      map2_df(.x = ., .y = marker_center,
-           ~ trim2d(.x, .y$xx, .y$yy, .y$ss) %>%
-             {tibble(mean = mean(.), median = median(.))})
-
-    # set outer_boundary to calculate pixel values nearby the marker
-    null_img <- array(0, dim = dim(trimed_img))
-    centers <- round(dim(trimed_img)[1:2] / 2)
-    outer_rect <-
-      null_img %>%
-      array_branch(margin = 3) %>%
-      map2(.x = ., .y = marker_center,
-              ~ pri::add_dimension(.x) %>%
-              overdraw(.y$xx, .y$yy, outer_size, 1)) %>%
-      abind::abind()
-
-    dilate_mask_size <-
-      round(size / 10) %>% {if_else(. %% 2 == 0, . + 1, .)} # to get odd number for the mask
-    non_marker <-
-      img_bin %>%
-      EBImage::dilate(EBImage::makeBrush(dilate_mask_size, shape="diamond")) %>%
-      EBImage::fillHull() %>%
-      `!`
-    target_pixel = sum(outer_rect * non_marker)
-
-    target_int <-
-      (trimed_img * outer_rect * non_marker) %>%
-      array_branch(margin = 3) %>%
-      map_df(function(mtrx){
-        mtrx[mtrx > 0] %>%
-          {tibble(mean = mean(., na.rm = T), median = median(., na.rm = T))}
-      })
-
-
-    if(show_mapping){
-      library(imager)
-
-      ref_map <-
-        array(0, dim = dim(trimed_img)) %>%
-        array_branch(margin = 3) %>%
-        map2(.x = ., .y = marker_center,
-             ~ pri::add_dimension(.x) %>%
-             overdraw(.y$xx, .y$yy, .y$ss, by = 1)) %>%
-        abind::abind()
-
-      target_map <-
-        outer_rect * non_marker
-
-      overlayed <-
-        trimed_img -  ref_map * .5 - target_map * .5
-
-      show(overlayed)
-    }
-
-
-    marker_center %>%
-      bind_rows %>%
-      transmute(x = xx + x - size, y = yy + y - size,
-                size_inner = ss, size_outer = outer_size,
-                pixel_marker = 4*ss^2, pixel_target = target_pixel) %>%
-      list(location = .,
-           marker = marker_int,
-           target = target_int,
-           reflectance = target_int / marker_int) %>%
-      return()
-  }
-
-
-marker_datum <-
-  function(index, loc, gamma, img = NULL, ...){
-
-    if(is.null(img)){
-      img <- index$path %>% read_imgs %>% `^`(., gamma)
-    }
-
-    duplicate_flag <- F
-
-    if(length(dim(img)) == 2){
-      duplicate_flag <- T
-      img <-
-        img %>%
-        pri::add_dimension() %>%
-        {list(., .)} %>%
-        abind::abind()
-    }
-
-    dat <-
-      loc %>%
-      split(.$location) %>%
-      markers(img, ., ...)
-
-    if(duplicate_flag){
-      dat <-
-        dat %>%
-        map(~ map(., ~ slice(., 1)))
-    }
-
-    list(dat = dat, index = select(index, group_vars(index)))
-  }
-
-marker_data <-
-  function(index, loc_sw, loc_lw, gamma, img_sw = NULL, img_lw = NULL, .prefixes = c("530", "570"), ...){
-    index_sw <- index %>% filter(prefix == .prefixes[1])
-    index_lw <- index %>% filter(prefix == .prefixes[2])
-
-    if(is.null(img_sw)){
-      img_sw <- index_sw$path %>% read_imgs %>% `^`(., gamma)
-    }
-    if(is.null(img_lw)){
-      img_lw <- index_lw$path %>% read_imgs %>% `^`(., gamma)
-    }
-
-    duplicate_flag_sw <- F
-    duplicate_flag_lw <- F
-
-    if(length(dim(img_sw)) == 2){
-      duplicate_flag_sw <- T
-      img_sw <-
-        img_sw %>%
-        add_dim %>%
-        {list(., .)} %>%
-        abind::abind()
-    }
-    if(length(dim(img_lw)) == 2){
-      duplicate_flag_lw <- T
-      img_lw <-
-        img_lw %>%
-        add_dim %>%
-        {list(., .)} %>%
-        abind::abind()
-    }
-
-    dat_sw <-
-      loc_sw %>%
-      split(.$location) %>%
-      markers(img_sw, ., ...)
-
-    dat_lw <-
-      loc_lw %>%
-      split(.$location) %>%
-      markers(img_lw, ., ...)
-
-    if(duplicate_flag_sw){
-      dat_sw <-
-        dat_sw %>%
-        map(~ map(., ~ slice(., 1)))
-    }
-    if(duplicate_flag_lw){
-      dat_lw <-
-        dat_lw %>%
-        map(~ map(., ~ slice(., 1)))
-    }
-
-    list(short = dat_sw, long = dat_lw,
-         short_index = select(index_sw, group_vars(index)),
-         long_index = select(index_lw, group_vars(index)))
-  }
-
-markers <-
-  function(imgs, list_locations, ...){
-    list_locations %>%
-      map(function(batch){
-        print(batch$location)
-        get_marker(imgs, x = batch$x, y = batch$y, size = batch$size, ...)
-      }
-      )
-  }
-
-
-marker2pri <-
-  function(list_marker_data){
-    info <-
-      list_marker_data[3:4] %>%
-      map(~ungroup(.) %>% select(-prefix))
-
-    if(identical(info[[1]], info[[2]])){
-      add_info <- mutate(info[[1]], frame = 1:dim(info[[1]])[1])
-    } else {
-      stop("two band images are not paired appropriately")
-    }
-
-    map2_df(list_marker_data[[1]], list_marker_data[[2]],
-            ~ calc_pri(.x$reflectance, .y$reflectance) %>%
-              mutate(frame = seq_along(mean)), .id = "location") %>%
-      left_join(., add_info, by = "frame")
-  }
-
-marker2param <-
-  function(list_marker_data){
-    info <-
-      list_marker_data[3:4] %>%
-      map(~ungroup(.) %>% select(-prefix))
-
-    if(identical(info[[1]], info[[2]])){
-      add_info <- mutate(info[[1]], frame = 1:dim(info[[1]])[1])
-    } else {
-      stop("two band images are not paired appropriately")
-    }
-
-    map2_df(list_marker_data[[1]], list_marker_data[[2]],
-            ~ calc_pri(.x$reflectance, .y$reflectance) %>%
-              mutate(marker_s = .x$marker$median, target_s = .x$target$median,
-                     marker_l = .y$marker$median, target_l = .y$target$median,
-                     frame = seq_along(mean)), .id = "location") %>%
-      left_join(., add_info, by = "frame")
-  }
+# # detect_marker
+# 
+# get_marker <-
+#   function(img, x, y, size, outer_size, percent = "95%", occupancy = .1, show_mapping = F){
+# 
+#     trimed_img <-
+#       trim(img, x, y, size)
+# 
+#     img_bin <-
+#       trimed_img %>%
+#         pri::add_dimension() %>%
+#         imager::threshold(percent) %>%
+#         # thresh(., w = size * window, h = size * window, offset) %>%
+#         .[,,,1] %>%
+#         EBImage::fillHull()
+# 
+#     marker_center <-
+#       img_bin %>%
+#       EBImage::bwlabel() %>%
+#       array_branch(margin = 3) %>%
+#       map(function(img_split){
+#           marker_detect <-
+#             computeFeatures.shape(img_split) %>%
+#             as_tibble %>%
+#             mutate(row_index = seq_along(s.area), area = s.area,
+#                    size_for_calc = round(s.radius.mean * .25)) %>%
+#             arrange(desc(area)) %>%
+#             filter(area > size^2 * occupancy^2)
+# 
+#           if(nrow(marker_detect) != 1){
+#             cat(paste0("markers may be splitted. detected ",
+#                        nrow(marker_detect),
+#                        " locations in binarized image.\n"))
+#           }
+# 
+#           computeFeatures.moment(img_split) %>%
+#             as_tibble %>%
+#             slice(marker_detect$row_index[1]) %>%
+#             transmute(xx = round(m.cx), yy = round(m.cy), ss = marker_detect$size_for_calc[1])
+#     })
+# 
+#     marker_int <-
+#       trimed_img %>%
+#       array_branch(margin = 3) %>%
+#       map2_df(.x = ., .y = marker_center,
+#            ~ trim2d(.x, .y$xx, .y$yy, .y$ss) %>%
+#              {tibble(mean = mean(.), median = median(.))})
+# 
+#     # set outer_boundary to calculate pixel values nearby the marker
+#     null_img <- array(0, dim = dim(trimed_img))
+#     centers <- round(dim(trimed_img)[1:2] / 2)
+#     outer_rect <-
+#       null_img %>%
+#       array_branch(margin = 3) %>%
+#       map2(.x = ., .y = marker_center,
+#               ~ pri::add_dimension(.x) %>%
+#               overdraw(.y$xx, .y$yy, outer_size, 1)) %>%
+#       abind::abind()
+# 
+#     dilate_mask_size <-
+#       round(size / 10) %>% {if_else(. %% 2 == 0, . + 1, .)} # to get odd number for the mask
+#     non_marker <-
+#       img_bin %>%
+#       EBImage::dilate(EBImage::makeBrush(dilate_mask_size, shape="diamond")) %>%
+#       EBImage::fillHull() %>%
+#       `!`
+#     target_pixel = sum(outer_rect * non_marker)
+# 
+#     target_int <-
+#       (trimed_img * outer_rect * non_marker) %>%
+#       array_branch(margin = 3) %>%
+#       map_df(function(mtrx){
+#         mtrx[mtrx > 0] %>%
+#           {tibble(mean = mean(., na.rm = T), median = median(., na.rm = T))}
+#       })
+# 
+# 
+#     if(show_mapping){
+#       library(imager)
+# 
+#       ref_map <-
+#         array(0, dim = dim(trimed_img)) %>%
+#         array_branch(margin = 3) %>%
+#         map2(.x = ., .y = marker_center,
+#              ~ pri::add_dimension(.x) %>%
+#              overdraw(.y$xx, .y$yy, .y$ss, by = 1)) %>%
+#         abind::abind()
+# 
+#       target_map <-
+#         outer_rect * non_marker
+# 
+#       overlayed <-
+#         trimed_img -  ref_map * .5 - target_map * .5
+# 
+#       show(overlayed)
+#     }
+# 
+# 
+#     marker_center %>%
+#       bind_rows %>%
+#       transmute(x = xx + x - size, y = yy + y - size,
+#                 size_inner = ss, size_outer = outer_size,
+#                 pixel_marker = 4*ss^2, pixel_target = target_pixel) %>%
+#       list(location = .,
+#            marker = marker_int,
+#            target = target_int,
+#            reflectance = target_int / marker_int) %>%
+#       return()
+#   }
+# 
+# 
+# marker_datum <-
+#   function(index, loc, gamma, img = NULL, ...){
+# 
+#     if(is.null(img)){
+#       img <- index$path %>% read_imgs %>% `^`(., gamma)
+#     }
+# 
+#     duplicate_flag <- F
+# 
+#     if(length(dim(img)) == 2){
+#       duplicate_flag <- T
+#       img <-
+#         img %>%
+#         pri::add_dimension() %>%
+#         {list(., .)} %>%
+#         abind::abind()
+#     }
+# 
+#     dat <-
+#       loc %>%
+#       split(.$location) %>%
+#       markers(img, ., ...)
+# 
+#     if(duplicate_flag){
+#       dat <-
+#         dat %>%
+#         map(~ map(., ~ slice(., 1)))
+#     }
+# 
+#     list(dat = dat, index = select(index, group_vars(index)))
+#   }
+# 
+# marker_data <-
+#   function(index, loc_sw, loc_lw, gamma, img_sw = NULL, img_lw = NULL, .prefixes = c("530", "570"), ...){
+#     index_sw <- index %>% filter(prefix == .prefixes[1])
+#     index_lw <- index %>% filter(prefix == .prefixes[2])
+# 
+#     if(is.null(img_sw)){
+#       img_sw <- index_sw$path %>% read_imgs %>% `^`(., gamma)
+#     }
+#     if(is.null(img_lw)){
+#       img_lw <- index_lw$path %>% read_imgs %>% `^`(., gamma)
+#     }
+# 
+#     duplicate_flag_sw <- F
+#     duplicate_flag_lw <- F
+# 
+#     if(length(dim(img_sw)) == 2){
+#       duplicate_flag_sw <- T
+#       img_sw <-
+#         img_sw %>%
+#         add_dim %>%
+#         {list(., .)} %>%
+#         abind::abind()
+#     }
+#     if(length(dim(img_lw)) == 2){
+#       duplicate_flag_lw <- T
+#       img_lw <-
+#         img_lw %>%
+#         add_dim %>%
+#         {list(., .)} %>%
+#         abind::abind()
+#     }
+# 
+#     dat_sw <-
+#       loc_sw %>%
+#       split(.$location) %>%
+#       markers(img_sw, ., ...)
+# 
+#     dat_lw <-
+#       loc_lw %>%
+#       split(.$location) %>%
+#       markers(img_lw, ., ...)
+# 
+#     if(duplicate_flag_sw){
+#       dat_sw <-
+#         dat_sw %>%
+#         map(~ map(., ~ slice(., 1)))
+#     }
+#     if(duplicate_flag_lw){
+#       dat_lw <-
+#         dat_lw %>%
+#         map(~ map(., ~ slice(., 1)))
+#     }
+# 
+#     list(short = dat_sw, long = dat_lw,
+#          short_index = select(index_sw, group_vars(index)),
+#          long_index = select(index_lw, group_vars(index)))
+#   }
+# 
+# markers <-
+#   function(imgs, list_locations, ...){
+#     list_locations %>%
+#       map(function(batch){
+#         print(batch$location)
+#         get_marker(imgs, x = batch$x, y = batch$y, size = batch$size, ...)
+#       }
+#       )
+#   }
+# 
+# 
+# marker2pri <-
+#   function(list_marker_data){
+#     info <-
+#       list_marker_data[3:4] %>%
+#       map(~ungroup(.) %>% select(-prefix))
+# 
+#     if(identical(info[[1]], info[[2]])){
+#       add_info <- mutate(info[[1]], frame = 1:dim(info[[1]])[1])
+#     } else {
+#       stop("two band images are not paired appropriately")
+#     }
+# 
+#     map2_df(list_marker_data[[1]], list_marker_data[[2]],
+#             ~ calc_pri(.x$reflectance, .y$reflectance) %>%
+#               mutate(frame = seq_along(mean)), .id = "location") %>%
+#       left_join(., add_info, by = "frame")
+#   }
+# 
+# marker2param <-
+#   function(list_marker_data){
+#     info <-
+#       list_marker_data[3:4] %>%
+#       map(~ungroup(.) %>% select(-prefix))
+# 
+#     if(identical(info[[1]], info[[2]])){
+#       add_info <- mutate(info[[1]], frame = 1:dim(info[[1]])[1])
+#     } else {
+#       stop("two band images are not paired appropriately")
+#     }
+# 
+#     map2_df(list_marker_data[[1]], list_marker_data[[2]],
+#             ~ calc_pri(.x$reflectance, .y$reflectance) %>%
+#               mutate(marker_s = .x$marker$median, target_s = .x$target$median,
+#                      marker_l = .y$marker$median, target_l = .y$target$median,
+#                      frame = seq_along(mean)), .id = "location") %>%
+#       left_join(., add_info, by = "frame")
+#   }
---
+++ R/piece_processing.R
@@ -0,0 +1,274 @@
+# detecting, extracting, and summarising marker consists of grey-white plate
+
+get_marker_geom <-
+  function(){}
+
+set_marker_geom <-
+  function(){}
+
+
+divide_piece <-
+  function(location, img){
+    if(nrow(location) != 1){
+      stop("Error in divide_piece: nrow(location) != 1.\n `locations` should be split")
+    }
+    piece <- trim(img, x = location$x, y = location$y, size = location$size)
+
+    # add geometrical attributes so that the pieces are able to be restored
+    attributes(piece)$geometry <-
+      lst(location, original_dim = dim(img))
+    # add information required for binarization
+    attributes(piece)$binarise <-
+      lst(white_ratio = .95,
+          erode_size = 1,
+          occupancy = .001)
+    # add information required for extraction
+    attributes(piece)$center <- NULL
+
+    # add information required for extraction
+    attributes(piece)$extract <-
+      lst(ref_white_ratio = 1/3,
+          margin_white_ratio = 1.2,
+          outer_white_ratio = 2)
+
+    return(piece)
+  }
+
+
+set_center <-
+  function(img, .check = F, white_ratio = NULL, occupancy = NULL, erode_size = NULL, ..., .verbose = F){
+    if(is.null(white_ratio)){
+      white_ratio <- attributes(img)$binarise$white_ratio
+    } else {
+      attributes(img)$binarise$white_ratio <- white_ratio
+    }
+
+    if(is.null(erode_size)){
+      erode_size <- attributes(img)$binarise$erode_size
+    } else {
+      attributes(img)$binarise$erode_size <- erode_size
+    }
+
+    if(is.null(occupancy)){
+      occupancy <- attributes(img)$binarise$occupancy
+    } else {
+      attributes(img)$binarise$occupancy <- occupancy
+    }
+
+    print(attributes(img))
+
+    img_bw <-
+      img %>%
+      binarise(., wr = white_ratio) %>%
+      set_bw_label(., es = erode_size)
+
+    if(.check){
+      show(img_bw, ...)
+      return(NULL)
+    }
+
+    centers <-
+      img_bw %>%
+      unstack %>%
+      map(~ set_bw_center(., ocp = occupancy, .verbose = .verbose))
+
+    print(centers)
+
+    attributes(img)$center <- centers
+
+    return(img)
+  }
+
+
+
+binarise <-
+  function(img_, wr){
+    if(length(dim(img_)) == 2){
+      img_ <- add_dim(img_)
+    }
+
+    img_ %>%
+      add_dim %>%
+      imager::threshold(paste0(wr * 100, "%")) %>%
+      .[,,,1] %>%
+      `*`(., 1) %>%
+      EBImage::fillHull() %>%
+      return()
+  }
+
+
+set_bw_label <-
+  function(img_bw, es){
+    img_bw %>%
+      EBImage::dilate(kern = EBImage::makeBrush(es, shape = "disc")) %>%
+      EBImage::bwlabel() %>%
+      return()
+  }
+
+set_bw_center <-
+  function(mtrx_bw, ocp, .verbose){
+    computed_shape_info <-
+      mtrx_bw %>%
+      EBImage::computeFeatures.shape() %>%
+      as_tibble
+
+    if(nrow(computed_shape_info) == 0){
+      stop("Error in detecting marker-center: no white area was found")
+    }
+
+    white_areas <-
+      computed_shape_info %>%
+      mutate(row_index = seq_along(s.area),
+             expected_size = dim(mtrx_bw)[1] * dim(mtrx_bw)[2] * ocp) %>%
+      filter(s.area > expected_size) %>%
+      arrange(desc(s.area))
+
+    if(nrow(white_areas) > 1 & .verbose){
+      warning("Warning in detecting marker-center: several white areas were detected")
+    }
+
+    computed_moment_info <-
+      EBImage::computeFeatures.moment(mtrx_bw) %>%
+      as_tibble
+
+    row_index_of_maximum_area <- white_areas$row_index[1]
+    radius_of_maximum_area <- white_areas$s.radius.mean[1]
+
+    computed_moment_info[row_index_of_maximum_area,] %>%
+      transmute(x = round(m.cx), y = round(m.cy), radius_white = radius_of_maximum_area) %>%
+      return()
+  }
+
+
+
+
+
+
+
+
+
+
+set_masks <-
+  function(img, .check = T, ref_white_ratio = NULL, margin_white_ratio = NULL, outer_white_ratio = NULL, ..., .verbose = F){
+    if(is.null(ref_white_ratio)){
+      ref_white_ratio <- attributes(img)$extract$ref_white_ratio
+    } else {
+      attributes(img)$extract$ref_white_ratio <- ref_white_ratio
+    }
+
+    if(is.null(margin_white_ratio)){
+      margin_white_ratio <- attributes(img)$extract$margin_white_ratio
+    } else {
+      attributes(img)$extract$margin_white_ratio <- margin_white_ratio
+    }
+
+    if(is.null(outer_white_ratio)){
+      outer_white_ratio <- attributes(img)$extract$outer_white_ratio
+    } else {
+      attributes(img)$extract$outer_white_ratio <- outer_white_ratio
+    }
+
+    if(.check){
+      check_mask(img) %>% show(...)
+      return(NULL)
+    }
+
+    return(img)
+  }
+
+mask_marker <-
+  function(img_attr, ref_wht, .fill = 0){
+    out_dim <- c(dim(img_attr)[1:2], 1)
+    attributes(img_attr)$center %>%
+      map(function(center_z){
+        array(.fill, dim = out_dim) %>%
+          overdraw(., x_cent = center_z$x, y_cent = center_z$y,
+                   size = center_z$radius_white * ref_wht, by = 1)
+      }) %>%
+      stack
+  }
+
+mask_target <-
+  function(img_attr, mar_wht, out_wht, .fill = 0){
+    out_dim <- c(dim(img_attr)[1:2], 1)
+    attributes(img_attr)$center %>%
+      map(function(center_z){
+        array(.fill, dim = out_dim) %>%
+          overdraw(., x_cent = center_z$x, y_cent = center_z$y,
+                   size = center_z$radius_white * out_wht, by = 1) %>%
+          overdraw(.,x_cent = center_z$x, y_cent = center_z$y,
+                   size = center_z$radius_white * mar_wht, by = .fill)
+      }) %>%
+      stack
+  }
+
+check_mask <-
+  function(img_attr, map_rgb = c(marker = 1, target = 3), map_int = c(marker = 1, target = 1)){
+
+    out_img <-
+      EBImage::Image(img_attr) %>%
+      EBImage::channel("rgb")
+
+    marker <- extract_marker(img_attr, .fill = 0)
+    target <- extract_target(img_attr, .fill = 0)
+
+    out_img[,,map_rgb[1],] <- out_img[,,map_rgb[1],] + marker * map_int[1]
+    out_img[,,map_rgb[2],] <- out_img[,,map_rgb[2],] + target * map_int[2]
+
+    attributes(out_img)$geometry <- attributes(img_attr)$geometry
+    return(out_img)
+  }
+
+extract_reflectance <-
+  function(img_attr){
+    marker <-
+      extract_marker(img_attr, NA) %>%
+      plyr::adply(.margins = 3, .fun = c(median_marker = median, mean_marker = mean), na.rm = T, .id = "file")
+    target <-
+      extract_target(img_attr, NA) %>%
+      plyr::adply(.margins = 3, .fun = c(median_target = median, mean_target = mean), na.rm = T, .id = "file")
+
+
+    inner_join(marker, target, by = c("file")) %>%
+      mutate(median_refl = median_target / median_marker,
+             mean_refl = mean_target / mean_marker,
+             z = seq_along(file))
+  }
+
+extract_marker <-
+  function(img_attr, .fill = NA){
+    extract_prm <- attributes(img_attr)$extract
+
+    img_attr * mask_marker(img_attr, extract_prm$ref_white_ratio, .fill)
+  }
+
+extract_target <-
+  function(img_attr, .fill = NA){
+    extract_prm <- attributes(img_attr)$extract
+
+    img_attr * mask_target(img_attr, extract_prm$margin_white_ratio, extract_prm$outer_white_ratio, .fill)
+  }
+
+map_full <-
+  function(list_pieces, full_size_img){
+
+    result <-
+      EBImage::Image(full_size_img) %>%
+      EBImage::channel("rgb")
+
+    for(i in seq_along(list_pieces)){
+      piece <- list_pieces[[i]]
+      piece_location <- attributes(piece)$geometry$location
+
+      x_range <-
+        piece_location %>%
+        {(.$x - .$size):(.$x + .$size)}
+      y_range <-
+        piece_location %>%
+        {(.$y - .$size):(.$y + .$size)}
+
+      result[x_range, y_range, ,] <- piece
+    }
+
+    return(result)
+  }
