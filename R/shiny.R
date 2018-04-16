f537d6aa73d902c8f94ba990916d01ff4f98127a
MurakamiKeach <m.keach.11@gmail.com>
2018-04-16 15:08:31 +0900

    doc: added roxygen lines

--- R/shiny.R
+++ R/shiny.R
@@ -1,3 +1,10 @@
+#' Determine affine coefficient interatcively
+#'
+#' @description
+#' Opens shiny application.
+#'
+#' @export
+#' @return this function does not return any object.
 shiny_affine <-
   function(){
     runGitHub("KeachMurakami/shiny_apps", subdir = "affine_transform")
